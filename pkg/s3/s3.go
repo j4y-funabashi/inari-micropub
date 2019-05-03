517ce223cda5287a3b3dd66f114690374abf0bab
Jay Robinson <me@j4y.co>
2019-05-03 17:13:42 +0100

    wip stuff

--- pkg/s3/s3.go
+++ pkg/s3/s3.go
@@ -2,11 +2,8 @@ package s3
 
 import (
 	"bytes"
-	"encoding/json"
 	"io"
 	"log"
-	"strings"
-	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
@@ -14,68 +11,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
-	"github.com/j4y_funabashi/inari-micropub/pkg/mf2"
-	"github.com/sirupsen/logrus"
 )
-
-func NewSaver(logger *logrus.Logger, s3Endpoint, S3KeyPrefix, S3Bucket string) func(event mf2.PostCreatedEvent) error {
-
-	sess, _ := session.NewSession(&aws.Config{
-		Region: aws.String(endpoints.EuCentral1RegionID),
-	})
-	if s3Endpoint != "" {
-		sess, _ = session.NewSession(&aws.Config{
-			Credentials:      credentials.NewStaticCredentials("foo", "var", ""),
-			Region:           aws.String(endpoints.EuCentral1RegionID),
-			Endpoint:         aws.String(s3Endpoint),
-			DisableSSL:       aws.Bool(true),
-			S3ForcePathStyle: aws.Bool(true),
-		})
-	}
-
-	// deps
-	uploader := s3manager.NewUploader(sess)
-
-	return func(event mf2.PostCreatedEvent) error {
-
-		eventjson := new(bytes.Buffer)
-		err := json.NewEncoder(eventjson).Encode(event)
-		if err != nil {
-			logger.
-				WithError(err).
-				WithField("event", event).
-				Error("failed to encode event to json")
-			return err
-		}
-		eventData := eventjson.String()
-
-		fileKey := strings.Trim(S3KeyPrefix, "/ ") + "/" + time.Now().Format("2006/") + event.EventVersion + "_" + event.EventID + ".json"
-
-		_, err = uploader.Upload(&s3manager.UploadInput{
-			Bucket: aws.String(S3Bucket),
-			Key:    aws.String(fileKey),
-			Body:   eventjson,
-			ACL:    aws.String("private"),
-		})
-		if err != nil {
-			logger.
-				WithError(err).
-				WithField("key", fileKey).
-				WithField("event", eventData).
-				WithField("bucket", S3Bucket).
-				Error("failed to upload event to s3")
-			return err
-		}
-
-		logger.
-			WithField("key", fileKey).
-			WithField("event", eventData).
-			WithField("bucket", S3Bucket).
-			Info("uploaded event to s3")
-
-		return nil
-	}
-}
 
 type Client struct {
 	s3Client   *s3.S3
@@ -84,13 +20,26 @@ type Client struct {
 	uploader   *s3manager.Uploader
 }
 
-func NewClient() (Client, error) {
-	s, _ := session.NewSession(&aws.Config{
-		Region: aws.String(endpoints.EuCentral1RegionID),
+func NewClient(s3Endpoint string) (Client, error) {
+
+	region := endpoints.EuCentral1RegionID
+
+	sess, _ := session.NewSession(&aws.Config{
+		Region: aws.String(region),
 	})
-	downloader := s3manager.NewDownloader(s)
-	uploader := s3manager.NewUploader(s)
-	s3Client := s3.New(s)
+	if s3Endpoint != "" {
+		sess, _ = session.NewSession(&aws.Config{
+			Credentials:      credentials.NewStaticCredentials("foo", "var", ""),
+			Region:           aws.String(region),
+			Endpoint:         aws.String(s3Endpoint),
+			DisableSSL:       aws.Bool(true),
+			S3ForcePathStyle: aws.Bool(true),
+		})
+	}
+
+	downloader := s3manager.NewDownloader(sess)
+	uploader := s3manager.NewUploader(sess)
+	s3Client := s3.New(sess)
 	return Client{
 		s3Client:   s3Client,
 		downloader: downloader,
@@ -139,12 +88,16 @@ func (client Client) ReadObject(key, bucket string) (*bytes.Buffer, error) {
 	return bytes.NewBuffer(buf.Bytes()), nil
 }
 
-func (client Client) WriteObject(key, bucket string, body io.Reader) error {
+func (client Client) WriteObject(key, bucket string, body io.Reader, isPrivate bool) error {
+	acl := "private"
+	if !isPrivate {
+		acl = "public-read"
+	}
 	_, err := client.uploader.Upload(&s3manager.UploadInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(key),
 		Body:   body,
-		ACL:    aws.String("private"),
+		ACL:    aws.String(acl),
 	})
 	if err != nil {
 		log.Printf("failed to upload to s3 %v", err)
