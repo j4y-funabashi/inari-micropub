6258b2ace48e148a59e0f5029a7fea010a2551b9
Jay Robinson <me@j4y.co>
2019-10-08 15:20:44 +0100

    admin

--- pkg/db/selecta.go
+++ pkg/db/selecta.go
@@ -102,9 +102,10 @@ med1.month, med1.count, published.published_count
 FROM
 (SELECT month, count(*) as count FROM media WHERE year = $1 GROUP BY month) med1
 LEFT JOIN
-(SELECT month,count(*) as published_count FROM media INNER JOIN media_published ON media.id = media_published.id WHERE year = $1 GROUP BY month) published
+(SELECT month,count(*) as published_count FROM media INNER JOIN media_published ON media.id = media_published.id WHERE year = $2 GROUP BY month) published
 ON med1.month = published.month
  ORDER BY month DESC;`,
+		year,
 		year,
 	)
 	if err != nil {
